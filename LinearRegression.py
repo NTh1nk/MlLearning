7ad118f8375a6a1bdab7e5c26fb2bd75cc225d6c
Johannes <Johannesslb@gmail.com>
2025-03-13 17:32:29 +0100

    came up with some data

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,3 +1,7 @@
+#Import depencies#
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression 
+#
+X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).reshape(-1, 1)  
+y = np.array([2.2, 4.7, 6.4, 8.9, 10.7, 12.2, 14, 16, 18])  
