80d59f08711e0101ee41fdacafca29d188e1077c
Johannes <Johannesslb@gmail.com>
2025-03-13 17:42:11 +0100

    First model

--- LinearRegression.py
+++ LinearRegression.py
@@ -12,7 +12,19 @@ model.fit(X, y)
 #Predict a num
 next_number = model.predict([[10]])
 
-
+#Predict a number
 print("Predicted next number:", next_number[0])
 print("Slope/Hældning (m/a):", model.coef_[0])
 print("Intercept/Skæringspunktet (b):", model.intercept_)
+
+#Visualise the model
+
+plt.scatter(X, y, color="blue", label="Training Data")
+
+plt.plot(X, model.predict(X), color="red", label="Prediction Line")
+
+plt.scatter([10], next_number, color="green", marker="x", s=100, label="Prediction for 10")
+plt.xlabel("Previous Number")
+plt.ylabel("Next Number")
+plt.legend()
+plt.show()
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+This is a learning repository. It is in English, for simplicity and unity
+
+Remeber to enter the virtual create/enter a virtual enviroment
+
+Enter ML-env/Scripts/activate
