54665872e81c2fc31cfc8351586f5a3cd7fb0129
Johannes <Johannesslb@gmail.com>
2025-03-13 17:53:17 +0100

    10 features

---
+++ LinearRegression/10Features.py
@@ -0,0 +1,6 @@
+#What if we had 10 features for each data point?
+
+#In this case, we are importing StandardScaler, and not matplotlib, as we cant visualise the data.
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
